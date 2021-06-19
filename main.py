bd67ca0414f89f7f270e1efbfca0b8fdc8a71222
SiddharthRajpal <siddharthrajpal09@gmail.com>
2021-06-20 00:02:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from replit import db
 import urllib
 import json
 from googleapiclient.discovery import build
-
-
-
+from discord.ext import commands
 client=discord.Client()
 mutelist = []
 bad_words = [
@@ -16,9 +14,12 @@ bad_words = [
 ]
 db["y"]=True
 
+
 @client.event
 async def on_ready():
   print("Ready")
+  await client.change_presence(activity=discord.Game(name=" .help"))
+
 @client.event
 async def on_message(message):
   youtube_id = "UCX6OQ3DkcsbYNE6H8uQQuVA"
@@ -48,12 +49,12 @@ async def on_message(message):
     db["y"]=True
     await message.channel.send("BadWordCheck is now on")
 
-  if message.author in mutelist and msg.startswith("0xFF2021"):
+  if message.author in mutelist and msg.startswith(#Hidden for server security):
     mutelist.remove(message.author)
     await message.channel.send(f"{message.author} has been unmuted for saying the secret word!!")
     #From Line no 19-38 is BadWordCheck
   if msg.startswith(".subs"):
-    api_key = #empty for safety reasons 
+    api_key = #Hidden for security purposes
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     request = youtube.channels().list(
@@ -70,7 +71,7 @@ async def on_message(message):
     await message.channel.send("Total subscriber count of AeroblazeYt:-")
     await message.channel.send("```"+u["subscriberCount"]+"```")
   if msg.startswith(".videos"):
-    api_key = #empty for safety reasons.
+    api_key = #Hidden For Security Purposes
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     request = youtube.channels().list(
@@ -84,15 +85,45 @@ async def on_message(message):
     x =response['items']
     y=x[0]
     u = y['statistics']
+    print(u)
     await message.channel.send("Total number of videos uploaded by Aeroblaze Yt:-")
     await message.channel.send("```"+u["videoCount"]+"```")
+  if msg.startswith(".mod"):
+      await message.channel.send("Your mod application has been submittted and a moderator/admin will review it and contact you shortly.")
+      mods = client.get_channel(842804602998226984)
+      await mods.send(f"{message.author} wants to be a moderator please contact them.")
+      
 
   if msg.startswith(".src"):
     await message.channel.send("Source code available on github :- https://github.com/SiddharthRajpal/AeroBot")
-    
+  if msg.startswith(".help"):
+    await message.channel.send(
+      """         ```                          AEROBOT BY RandomPotato
+Welcome to this message. Aerobot is a custom bot that I have built specifically for this server using python. I will tell you about the commands and uses of Aerobot
 
+.subs :- Sends the current subscribers of our youtube channel
+.videos :- Sends the total number of videos uploaded to our youtube channel
+
+There is a constant check on bad words in this server via Aerobot, if you use any bad word you will be muted for 5 minutes
+
+.offcheck :- turns BadWordCheck off
+.oncheck :- turns BadWordCheck on (if it is off)
+
+Moderators and admins are given a secret keyword through which they can bypass the 5 minute mute.
+
+.src :- view link for the source code
+.help :- Get help
+
+
+
+Thank you. If there are any recommendations for features of the bot feel free to share.
+```"""
+    
+    )
+
+    
 
     
 
 keep_alive()
-client.run(#empty for safet reasons)
+client.run(#Hidden For Security Purposes)
