29c08de09d29e11448507285266471d884a75132
SiddharthRajpal <siddharthrajpal09@gmail.com>
2021-06-28 00:26:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,131 +1,120 @@
 import discord
 import asyncio
 from keep_alive import keep_alive
-from replit import db
 import urllib
 import json
 from googleapiclient.discovery import build
+from replit import db
 from discord.ext import commands
-client=discord.Client()
+import random
+
+client = commands.Bot(command_prefix =".")
+client.remove_command("help")
 mutelist = []
 bad_words = [
   "kutta",
   "kamina"
 ]
-db["y"]=True
-
-
+db["y"] = True
+youtube_id = "UCX6OQ3DkcsbYNE6H8uQQuVA"
 @client.event
 async def on_ready():
   print("Ready")
-  await client.change_presence(activity=discord.Game(name=" .help"))
 
-@client.event
-async def on_message(message):
-  youtube_id = "UCX6OQ3DkcsbYNE6H8uQQuVA"
+@client.command()
+async def ping(ctx):
+  await ctx.channel.send(f"{round(client.latency * 1000)} MS")
 
-  if message.author == client.user:
-    return
-  msg=message.content
-  waitTime=300 
-  if message.author in mutelist:
-    await message.delete()
-  if any(word in msg.lower() for word in bad_words) and db["y"]==True:
-    await message.delete()
-    mutelist.append(message.author)
-    await message.channel.send(f"{message.author} just said a bad word :angry: :triumph:")
-    await asyncio.sleep(1)
-    await message.channel.send(f"{message.author} has been muted in this server for 5 minutes")
-    await asyncio.sleep(waitTime)
-    mutelist.remove(message.author)
-    mutelist.remove(message.author)
-    await message.channel.send(f"** Mute for {message.author} has ended you may speak again**")
-  if msg.startswith(".offcheck"):
+@client.command()
+async def subs(ctx):
   
-    db["y"]=False
-    await message.channel.send("BadWordCheck is now off")
-  if msg.startswith(".oncheck"):
-    
-    db["y"]=True
-    await message.channel.send("BadWordCheck is now on")
+  api_key = #Hidden For Security Reasons
+  youtube = build('youtube', 'v3', developerKey=api_key)
 
-  if message.author in mutelist and msg.startswith(#Hidden for server security):
-    mutelist.remove(message.author)
-    await message.channel.send(f"{message.author} has been unmuted for saying the secret word!!")
-    #From Line no 19-38 is BadWordCheck
-  if msg.startswith(".subs"):
-    api_key = #Hidden for security purposes
-    youtube = build('youtube', 'v3', developerKey=api_key)
-
-    request = youtube.channels().list(
-        part='statistics',
-        id=youtube_id
+  request = youtube.channels().list(
+  part='statistics',
+  id=youtube_id
     )
+  response = request.execute()
+  #print(type(response))
+  #print(response)
+  x =response['items']
+  y=x[0]
+  u = y['statistics']
+  await ctx.channel.send("Total subscriber count of AeroblazeYt:-")
+  await ctx.channel.send("```"+u["subscriberCount"]+"```")
 
-    response = request.execute()
-    print(type(response))
-    print(response)
-    x =response['items']
-    y=x[0]
-    u = y['statistics']
-    await message.channel.send("Total subscriber count of AeroblazeYt:-")
-    await message.channel.send("```"+u["subscriberCount"]+"```")
-  if msg.startswith(".videos"):
-    api_key = #Hidden For Security Purposes
-    youtube = build('youtube', 'v3', developerKey=api_key)
+@client.command()
+async def videos(ctx):
+  api_key = #Hidden For Security Reasons
+  youtube = build('youtube', 'v3', developerKey=api_key)
 
-    request = youtube.channels().list(
-        part='statistics',
-        id=youtube_id
+  request = youtube.channels().list(
+  part='statistics',
+  id=youtube_id
     )
+  response = request.execute()
+  #print(type(response))
+  #print(response)
+  x =response['items']
+  y=x[0]
+  u = y['statistics']
+  #print(u)
+  await ctx.channel.send("Total number of videos uploaded by Aeroblaze Yt:-")
+  await ctx.channel.send("```"+u["videoCount"]+"```")
 
-    response = request.execute()
-    print(type(response))
-    print(response)
-    x =response['items']
-    y=x[0]
-    u = y['statistics']
-    print(u)
-    await message.channel.send("Total number of videos uploaded by Aeroblaze Yt:-")
-    await message.channel.send("```"+u["videoCount"]+"```")
-  if msg.startswith(".mod"):
-      await message.channel.send("Your mod application has been submittted and a moderator/admin will review it and contact you shortly.")
-      mods = client.get_channel(842804602998226984)
-      await mods.send(f"{message.author} wants to be a moderator please contact them.")
-      
+@client.command()
+async def src(ctx):
+  await ctx.channel.send("Source code available on github :- https://github.com/SiddharthRajpal/AeroBot")
 
-  if msg.startswith(".src"):
-    await message.channel.send("Source code available on github :- https://github.com/SiddharthRajpal/AeroBot")
-  if msg.startswith(".help"):
-    await message.channel.send(
-      """         ```                          AEROBOT BY RandomPotato
+
+@client.command()
+async def mod(ctx,*,reason):
+  await ctx.channel.send("Your mod application has been submittted and a moderator/admin will review it and contact you shortly.")
+  mods = client.get_channel(842804602998226984)
+  await mods.send(f"@{ctx.author} wants to be a moderator for the following reason :- **'{reason}'.** Please contact them and review their application.")
+
+@client.command(aliases = ['8ball'])
+async def _8ball(ctx, *, question="None"):
+
+  responses = [
+  "As I see it, yes.", 
+  "Ask again later.", 
+  "Better not tell you now.", 
+  "Cannot predict now.", 
+  "Concentrate and ask again.",
+  "Don’t count on it.", 
+  "It is certain.", 
+  "It is decidedly so.", 
+  "Most likely.", 
+  "My reply is no.", 
+  "No",
+  ""
+  "My sources say no.",
+  "Outlook not so good.", 
+  "Outlook good.", 
+  "Reply hazy, try again.", 
+  "Signs point to yes.",
+  "Very doubtful.", 
+  "Without a doubt.",
+  "Yes.", 
+  "Yes – definitely.", 
+  "You may rely on it."]
+  await ctx.channel.send(f"```Question :- {question} \nAnswer :- {random.choice(responses)}``` ")
+
+
+@client.command()
+async def help(ctx):
+  await ctx.channel.send(
+      """         ```           AEROBOT BY RandomPotato
 Welcome to this message. Aerobot is a custom bot that I have built specifically for this server using python. I will tell you about the commands and uses of Aerobot
 
 .subs :- Sends the current subscribers of our youtube channel
 .videos :- Sends the total number of videos uploaded to our youtube channel
-
-There is a constant check on bad words in this server via Aerobot, if you use any bad word you will be muted for 5 minutes
-
-.offcheck :- turns BadWordCheck off
-.oncheck :- turns BadWordCheck on (if it is off)
-
-Moderators and admins are given a secret keyword through which they can bypass the 5 minute mute.
-
 .src :- view link for the source code
 .help :- Get help
-.mod :- Request for moderator acess
-
-
-
-
-Thank you. If there are any recommendations for features of the bot feel free to share.
-```"""
-    
-    )
-
-    
-
-    
-
+.8ball :- Ask a Question
+.mod :- Request for moderator access (syntax = .mod reason)
+If there are any suggestions or queries or bugs please feel free to DM me :)```""")
 keep_alive()
-client.run(#Hidden For Security Purposes)
+client.run(#Hidden For security reasons)
